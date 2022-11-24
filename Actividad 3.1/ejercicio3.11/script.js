96b23504dd32202774eddec020ecdfaea3033e24
samU13 <samu13lop@gmail.com>
2022-11-24 14:26:08 +0100

    ajustes

--- Actividad 3.1/ejercicio3.11/script.js
+++ Actividad 3.1/ejercicio3.11/script.js
@@ -1,20 +1,29 @@
 "use stric";
 
-const boton1 = document.getElementById("I");
-const boton2 = document.getElementById("II");
-const boton3 = document.getElementById("III");
-const boton4 = document.getElementById("IV");
-const boton5 = document.getElementById("V");
-const boton6 = document.getElementById("VI");
-const boton7 = document.getElementById("VII");
-const boton8 = document.getElementById("VIII");
-const boton9 = document.getElementById("IX");
-const boton10 = document.getElementById("X");
-const boton11 = document.getElementById("XI");
+// const boton1 = document.getElementById("I");
+// const boton2 = document.getElementById("II");
+// const boton3 = document.getElementById("III");
+// const boton4 = document.getElementById("IV");
+// const boton5 = document.getElementById("V");
+// const boton6 = document.getElementById("VI");
+// const boton7 = document.getElementById("VII");
+// const boton8 = document.getElementById("VIII");
+// const boton9 = document.getElementById("IX");
+// const boton10 = document.getElementById("X");
+// const boton11 = document.getElementById("XI");
 
 const resultado = document.getElementById("resultado");
 
 const limpiar = () => (resultado.innerHTML = " ");
+
+const separar = function () {
+  let datos = String(document.getElementById("nombre").value);
+  const separar = datos.split(",");
+  const sinEspacios = separar.map((nombre) => {
+    return nombre.trim();
+  });
+  return sinEspacios;
+};
 
 function opcion1() {
   limpiar();
@@ -37,12 +46,11 @@ function opcion1() {
 function opcion2() {
   limpiar();
   let comprobar = opcion1();
-  console.log(comprobar);
   if (!comprobar) {
-    let datos = String(document.getElementById("nombre").value);
-    const sinEspacios = datos.trim().replace(" ,", ",").replace(",", ", ");
-    resultado.innerHTML = sinEspacios;
-    return sinEspacios;
+    const nombreApellidos = separar();
+    const stringNombre = `${nombreApellidos[0]}, ${nombreApellidos[1]}`;
+    resultado.innerHTML = stringNombre;
+    return stringNombre;
   } else {
     resultado.innerHTML = "Datos erroneos";
   }
@@ -51,12 +59,12 @@ function opcion2() {
 function opcion3() {
   limpiar();
   let comprobar = opcion1();
-  const nombre = opcion2();
-  console.log(nombre);
-  const partes = nombre.split(" ");
-  console.log(partes);
-  const saludo = `Hola ${partes[partes.length - 2]}`;
+
   if (!comprobar) {
+    const nombreApellidos = separar();
+    const nombre = nombreApellidos[1].split(" ");
+    const primerNombre = nombre[0];
+    const saludo = `Hola ${primerNombre}`;
     resultado.innerHTML = saludo;
   } else {
     resultado.innerHTML = "Datos erroneos";
@@ -66,9 +74,10 @@ function opcion3() {
 function opcion4() {
   limpiar();
   let comprobar = opcion1();
-  const nombre = opcion2();
-  const longitud = nombre.length - 2;
+
   if (!comprobar) {
+    const nombreApellidos = separar();
+    const longitud = nombreApellidos[0].length + nombreApellidos[1].length;
     resultado.innerHTML = longitud;
   } else {
     resultado.innerHTML = "Datos erroneos";
@@ -78,9 +87,10 @@ function opcion4() {
 function opcion5() {
   limpiar();
   let comprobar = opcion1();
-  const nombre = opcion2();
-  const minusculas = nombre.toLowerCase();
+
   if (!comprobar) {
+    const nombre = opcion2();
+    const minusculas = nombre.toLowerCase();
     resultado.innerHTML = minusculas;
   } else {
     resultado.innerHTML = "Datos erroneos";
@@ -90,10 +100,12 @@ function opcion5() {
 function opcion6() {
   limpiar();
   let comprobar = opcion1();
-  const nombre = opcion2();
-  const mayusculas = nombre.toUpperCase();
+
   if (!comprobar) {
+    const nombre = opcion2();
+    const mayusculas = nombre.toUpperCase();
     resultado.innerHTML = mayusculas;
+    return mayusculas;
   } else {
     resultado.innerHTML = "Datos erroneos";
   }
@@ -102,14 +114,10 @@ function opcion6() {
 function opcion7() {
   limpiar();
   let comprobar = opcion1();
-  const nombre = opcion2();
-  console.log(nombre);
-  const partes = nombre.split(" ");
-  console.log(partes);
-  const totalNombre =
-    partes[partes.length - 1].length + partes[partes.length - 2].length;
-  console.log(totalNombre);
+
   if (!comprobar) {
+    const nombreApellidos = separar();
+    const totalNombre = nombreApellidos[1].length;
     resultado.innerHTML = totalNombre;
   } else {
     resultado.innerHTML = "Datos erroneos";
@@ -119,14 +127,69 @@ function opcion7() {
 function opcion8() {
   limpiar();
   let comprobar = opcion1();
-  const nombre = opcion2();
-  console.log(nombre);
-  const partes = nombre.split(" ");
-  console.log(partes);
-  const totalApellidos = partes[0].length + partes[1].length;
-  console.log(totalApellidos);
   if (!comprobar) {
+    const nombreApellidos = separar();
+    const totalApellidos = nombreApellidos[0].length;
+    console.log(totalApellidos);
     resultado.innerHTML = totalApellidos;
+  } else {
+    resultado.innerHTML = "Datos erroneos";
+  }
+}
+
+function opcion9() {
+  limpiar();
+  let comprobar = opcion1();
+  if (!comprobar) {
+    const nombreApellidos = separar();
+    const separadoNombre = nombreApellidos[1].split(" ");
+    const separadoApellidos = nombreApellidos[0].split(" ");
+    let separados = separadoApellidos.concat(separadoNombre);
+    const iniciales = [];
+    for (let i = 0; i <= separados.length - 1; i++) {
+      iniciales.push(separados[i].charAt(0).toUpperCase());
+    }
+    resultado.innerHTML = `${iniciales.join(".")}`;
+    return iniciales;
+  } else {
+    resultado.innerHTML = "Datos erroneos";
+  }
+}
+
+function opcion10() {
+  limpiar();
+  let comprobar = opcion1();
+  if (!comprobar) {
+    const nombreApellidos = separar();
+    const separadoNombre = nombreApellidos[1].split(" ");
+    const separadoApellidos = nombreApellidos[0].split(" ");
+    let separados = separadoApellidos.concat(separadoNombre);
+    let encriptado = [];
+    for (let i = 0; i <= separados.length - 1; i++) {
+      for (let x = 1; x <= separados[i].length; x++) {
+        encriptado.push("*");
+      }
+      encriptado.push(" ");
+    }
+    resultado.innerHTML = encriptado.join("");
+  } else {
+    resultado.innerHTML = "Datos erroneos";
+  }
+}
+
+function opcion11() {
+  limpiar();
+  let comprobar = opcion1();
+  if (!comprobar) {
+    const ini = opcion9();
+    const reverse = ini.slice().reverse();
+    const iniciales = ini.join("");
+    const inicialesReves = reverse.join("");
+    if (iniciales != inicialesReves) {
+      resultado.innerHTML = "No es palindromo";
+    } else {
+      resultado.innerHTML = "Es palindromo";
+    }
   } else {
     resultado.innerHTML = "Datos erroneos";
   }
